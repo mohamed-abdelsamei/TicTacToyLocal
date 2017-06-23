09426f93676f73fb209469fbafcaf12a0de67d8d
MOHAMED ESSAM <ENG.MOHAMED3SSAM@GMAIL.com>
2017-06-23 15:58:37 +0200

    add toast message from clicked btn id

--- app/src/main/java/com/c0d3r/tictactoylocal/tictactoylocal/MainActivity.kt
+++ app/src/main/java/com/c0d3r/tictactoylocal/tictactoylocal/MainActivity.kt
@@ -15,7 +15,19 @@ class MainActivity : AppCompatActivity() {
 
     fun btnClick(view: View){
         val btnSelected = view as Button
+        var cellID = 0
+        when(btnSelected.id){
+            R.id.btn1 -> cellID=1
+            R.id.btn2 -> cellID=2
+            R.id.btn3 -> cellID=3
+            R.id.btn4 -> cellID=4
+            R.id.btn5 -> cellID=5
+            R.id.btn6 -> cellID=6
+            R.id.btn7 -> cellID=7
+            R.id.btn8 -> cellID=8
+            R.id.btn9 -> cellID=9
 
-        Toast.makeText(this,btnSelected.id.toString(), Toast.LENGTH_LONG).show()
+        }
+        Toast.makeText(this,"ID "+cellID, Toast.LENGTH_LONG).show()
     }
 }
